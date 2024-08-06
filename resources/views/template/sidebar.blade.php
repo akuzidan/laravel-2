4010d2bacd039dd2a0f4c23146e366ebd2ec4ef4
zidan <ruslisiswanto124@gmail.com>
2024-08-06 22:10:36 +0700

    ganti jeneng ku

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -14,7 +14,7 @@
           <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Arif</a>
+          <a href="#" class="d-block">Zidan</a>
         </div>
       </div>
 
